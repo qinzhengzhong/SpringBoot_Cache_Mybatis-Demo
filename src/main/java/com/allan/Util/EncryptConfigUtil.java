8a8a2a50ca43245cf619646c90f69f4d0fe685b3
qinzhengzhong <qinzz_allan@sina.com>
2020-03-21 12:29:22 +0800

    提交

---
+++ src/main/java/com/allan/Util/EncryptConfigUtil.java
@@ -0,0 +1,24 @@
+package com.allan.Util;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 生成数据库密码密文工具
+ *
+ * @Auther: qinzhengzhong
+ * @Date: 2020/3/21 11:57
+ * @Description:
+ */
+public class EncryptConfigUtil {
+    @Autowired
+    StringEncryptor stringEncryptor;//密码解码器自动注入
+
+
+    @org.junit.Test
+    public void encryptPwd() {
+        String possword="Qinzz@allan123";
+        String result = stringEncryptor.encrypt(possword);
+        System.out.println(result);
+    }
+}
--- src/main/java/test/TestBootTest.java
+++ src/main/java/test/TestBootTest.java
@@ -1,6 +1,8 @@
 package test;
 
 import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TestBootTest {
@@ -9,8 +11,37 @@ public class TestBootTest {
 
     @org.junit.Test
     public void encryptPwd() {
+        ininPass();
+    }
+
+    public static void main(String[] args) {
+
+        String password = "qinzz@allan!";
+        String data = "root";
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName(null);
+        config.setProviderClassName(null);
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+//        config.setIvGeneratorClassName("org.jasypt.salt.NoOpIVGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        System.out.println(encryptor.encrypt(data));
+        System.out.println(encryptor.encrypt(password));
+
+
+    }
+
+    public void ininPass(){
         String possword="qinzz@allan!";
         String result = stringEncryptor.encrypt(possword);
         System.out.println(result);
     }
+
 }
