a6a79f6e05de2b9709ff96abab6911c4f13a783a
qinzhengzhong <qinzz_allan@sina.com>
2020-08-16 15:26:50 +0800

    添加rabbitmq

--- src/main/java/test/RabbitMqTest.java
+++ src/main/java/test/RabbitMqTest.java
@@ -20,35 +20,38 @@ public class RabbitMqTest {
 
     @Test
     public void send() throws Exception {
+        try {
+            //创建连接工程
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("127.0.0.1");
+            factory.setPort(5672);
+            factory.setUsername("admin");
+            factory.setPassword("admin");
+            //创建连接
+            Connection connection = factory.newConnection();
 
-        //创建连接工程
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("127.0.0.1");
-        factory.setPort(5672);
-        factory.setUsername("admin");
-        factory.setPassword("admin");
-        //创建连接
-        Connection connection = factory.newConnection();
+            //创建消息通道
+            Channel channel = connection.createChannel();
 
-        //创建消息通道
-        Channel channel = connection.createChannel();
-
-        //生成一个消息队列
-        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+            //生成一个消息队列
+            channel.queueDeclare(QUEUE_NAME, true, false, false, null);
 
 
-        for (int i = 0; i < 10; i++) {
-            String message = "Hello World RabbitMQ count: " + i;
+            for (int i = 0; i < 10; i++) {
+                String message = "Hello World RabbitMQ count: " + i;
 
-            //发布消息，第一个参数表示路由（Exchange名称），未""则表示使用默认消息路由
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+                //发布消息，第一个参数表示路由（Exchange名称），未""则表示使用默认消息路由
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
 
-            System.out.println(" [x] Sent '" + message + "'");
+                System.out.println(" [x] Sent '" + message + "'");
+            }
+
+            //关闭消息通道和连接
+            channel.close();
+            connection.close();
+        }catch (Exception e){
+            e.printStackTrace();
         }
-
-        //关闭消息通道和连接
-        channel.close();
-        connection.close();
 
     }
 
